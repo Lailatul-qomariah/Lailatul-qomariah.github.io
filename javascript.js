c41891228c0634fbaedaf1a784c11b4d1324639f
Lailatul-qomariah <lailatulqomariah165@gmail.com>
2019-12-17 11:39:58 +0700

    update projek terbaru

--- javascript.js
+++ javascript.js
@@ -3,24 +3,28 @@ function tombol(){
 		var Linky = document.getElementById("tombol");
 		Linky.onclick = login.style.display = "block";}
 
-	function klik(){
-		if (document.getElementById('nama').value == ""){
-	            alert("Nama Tidak Boleh Kosong");
-	            return false;
-	        }
+function close(){
+	var Formnya = document.getElementById('box');
+	var Tutup = document.getElementsByClassName("close")[0];
+	Tutup.onclick =login.style.display = "none";
+}
+function klik(){
+	if (document.getElementById('nama').value == ""){
+            alert("Nama Tidak Boleh Kosong");
+            return false;
+        }
 	            
 
-	    if (document.getElementById('email').value == ""){
-	            alert("Email Tidak Boleh Kosong");
-	            return false;
-	        }
+    if (document.getElementById('email').value == ""){
+            alert("Email Tidak Boleh Kosong");
+            return false;
+        }
 
-
-	     if (document.getElementById('password').value == ""){
-	            alert("Password Tidak Boleh Kosong");
-	            return false;
-	        }
-	        else{
-	        	location.href="translate.html"
-	        }
-	    }
+     if (document.getElementById('password').value == ""){
+            alert("Password Tidak Boleh Kosong");
+            return false;
+        }
+        else{
+        	location.href="translate.html"
+        }
+    }
